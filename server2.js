e304b2af2366292cbea975f9e63ee49c7c33e419
UthmanQureshi <uthman.aq@gmail.com>
2023-10-26 15:36:40 -0400

    allowed port access

--- server2.js
+++ server2.js
@@ -286,6 +286,6 @@ app.delete('/books/:bookId/lines/:lineId', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
