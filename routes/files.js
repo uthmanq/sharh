29b503c3b06d5e2099371ac33467f10563cc6cba
Uthman Qureshi <uthman.aq@gmail.com>
2025-01-25 20:01:58 -0500

    added visibility and sorting by visibility

--- routes/files.js
+++ routes/files.js
@@ -5,193 +5,197 @@ const File = require('../models/File'); // The Mongoose File model
 const multer = require('multer'); // Import multer for file uploads
 const authenticateToken = require('../middleware/authenticate')
 const { v4: uuidv4 } = require('uuid'); // Use UUID for unique key generation
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = process.env.SECRET_KEY;
+const User = require('../models/User'); // Ensure you have the User model imported
 
 // Set up multer to store files temporarily on the server
 const upload = multer({ dest: '../uploads/' }); // Files will be temporarily stored in 'uploads/' folder
 
 // Route to upload a file to S3 and save metadata in MongoDB
 router.post('/upload', authenticateToken(['admin']), upload.single('file'), async (req, res) => {
-  try {
-      const file = req.file; // The uploaded file
-      const author = req.body.author || 'Unknown'; // Optional author field
-      const title = req.body.title || file.originalname; // User-provided title or default to original name
-      const tags = req.body.tags ? req.body.tags.split(',').map(tag => tag.trim()) : []; // Extract and split tags
-      const categories = req.body.categories ? req.body.categories.split(',').map(cat => cat.trim()) : ['uncategorized']; // Extract and split categories
+    try {
+        const file = req.file; // The uploaded file
+        const author = req.body.author || 'Unknown'; // Optional author field
+        const title = req.body.title || file.originalname; // User-provided title or default to original name
+        const tags = req.body.tags ? req.body.tags.split(',').map(tag => tag.trim()) : []; // Extract and split tags
+        const categories = req.body.categories ? req.body.categories.split(',').map(cat => cat.trim()) : ['uncategorized']; // Extract and split categories
+        const visibility = req.body.visibility;
+        if (!file) {
+            return res.status(400).json({ message: 'No file uploaded' });
+        }
 
-      if (!file) {
-          return res.status(400).json({ message: 'No file uploaded' });
-      }
+        // Generate a unique key for S3 by appending a timestamp or UUID
+        const uniqueKey = `uploads/${uuidv4()}-${file.originalname}`; // Generates a unique key using UUID
 
-      // Generate a unique key for S3 by appending a timestamp or UUID
-      const uniqueKey = `uploads/${uuidv4()}-${file.originalname}`; // Generates a unique key using UUID
+        // Upload file to S3
+        const s3Key = `uploads/${file.originalname}`; // The key (file name + path in S3)
+        const s3Data = await s3Service.uploadFile(file.path, uniqueKey); // File uploaded from temporary path
 
-      // Upload file to S3
-      const s3Key = `uploads/${file.originalname}`; // The key (file name + path in S3)
-      const s3Data = await s3Service.uploadFile(file.path, uniqueKey); // File uploaded from temporary path
+        // Save file metadata in MongoDB
+        const newFile = new File({
+            fileName: title,
+            s3Key: s3Data.Key,
+            author: author,
+            s3Bucket: s3Data.Bucket,
+            fileSize: file.size, // File size from multer
+            fileType: file.mimetype, // MIME type from multer
+            tags: tags, // Add tags to the file metadata
+            categories: categories, // Add categories to the file metadata
+            visibility: visibility
+        });
 
-      // Save file metadata in MongoDB
-      const newFile = new File({
-          fileName: title, 
-          s3Key: s3Data.Key,
-          author: author,
-          s3Bucket: s3Data.Bucket,
-          fileSize: file.size, // File size from multer
-          fileType: file.mimetype, // MIME type from multer
-          tags: tags, // Add tags to the file metadata
-          categories: categories, // Add categories to the file metadata
-      });
+        await newFile.save();
 
-      await newFile.save();
-
-      res.status(201).json({ message: 'File uploaded and metadata saved successfully', file: newFile });
-  } catch (err) {
-      console.error('Error in upload:', err);
-      res.status(500).json({ message: 'Error uploading file', error: err.message });
-  }
+        res.status(201).json({ message: 'File uploaded and metadata saved successfully', file: newFile });
+    } catch (err) {
+        console.error('Error in upload:', err);
+        res.status(500).json({ message: 'Error uploading file', error: err.message });
+    }
 });
 
 
 // Route to download the file from S3 using the File model's ID
-router.get('/download/:id',authenticateToken(['member', 'admin']), async (req, res) => {
-  try {
-      const fileId = req.params.id;
+router.get('/download/:id', authenticateToken(['member', 'admin']), async (req, res) => {
+    try {
+        const fileId = req.params.id;
 
-      // Fetch the file metadata from MongoDB by ID
-      const fileRecord = await File.findById(fileId);
+        // Fetch the file metadata from MongoDB by ID
+        const fileRecord = await File.findById(fileId);
 
-      if (!fileRecord) {
-          return res.status(404).json({ message: 'File not found' });
-      }
+        if (!fileRecord) {
+            return res.status(404).json({ message: 'File not found' });
+        }
 
-      const s3Key = fileRecord.s3Key;
+        const s3Key = fileRecord.s3Key;
 
-      // Retrieve the file stream from S3
-      const fileStream = s3Service.getFileStream(s3Key);
+        // Retrieve the file stream from S3
+        const fileStream = s3Service.getFileStream(s3Key);
 
-      // Set the Content-Type header based on the file type
-      res.setHeader('Content-Type', fileRecord.fileType || 'application/octet-stream');
+        // Set the Content-Type header based on the file type
+        res.setHeader('Content-Type', fileRecord.fileType || 'application/octet-stream');
 
-      // Ensure the file is downloaded by setting Content-Disposition header
-      //res.setHeader('Content-Disposition', `attachment; filename="${fileRecord.fileName}"`);
+        // Ensure the file is downloaded by setting Content-Disposition header
+        //res.setHeader('Content-Disposition', `attachment; filename="${fileRecord.fileName}"`);
 
-      // Pipe the S3 file stream to the response
-      fileStream.pipe(res);
-  } catch (err) {
-      console.error('Error retrieving file:', err);
-      res.status(500).json({ message: 'Error retrieving file', error: err.message });
-  }
+        // Pipe the S3 file stream to the response
+        fileStream.pipe(res);
+    } catch (err) {
+        console.error('Error retrieving file:', err);
+        res.status(500).json({ message: 'Error retrieving file', error: err.message });
+    }
 });
 
 // Route to generate a pre-signed URL for viewing the file
 router.get('/view/:id', authenticateToken(['member', 'admin']), async (req, res) => {
     try {
-      const fileId = req.params.id;
-  
-      // Fetch the file metadata from MongoDB by ID
-      const fileRecord = await File.findById(fileId);
-  
-      if (!fileRecord) {
-        return res.status(404).json({ message: 'File not found' });
-      }
-  
-      const s3Key = fileRecord.s3Key;
-  
-      // Generate a pre-signed URL for viewing
-      const presignedUrl = await s3Service.getPresignedUrl(s3Key, 300); // URL valid for 5 minutes
-  
-      res.json({ url: presignedUrl });
+        const fileId = req.params.id;
+
+        // Fetch the file metadata from MongoDB by ID
+        const fileRecord = await File.findById(fileId);
+
+        if (!fileRecord) {
+            return res.status(404).json({ message: 'File not found' });
+        }
+
+        const s3Key = fileRecord.s3Key;
+
+        // Generate a pre-signed URL for viewing
+        const presignedUrl = await s3Service.getPresignedUrl(s3Key, 300); // URL valid for 5 minutes
+
+        res.json({ url: presignedUrl });
     } catch (err) {
-      console.error('Error generating pre-signed URL:', err);
-      res.status(500).json({ message: 'Error generating view link', error: err.message });
+        console.error('Error generating pre-signed URL:', err);
+        res.status(500).json({ message: 'Error generating view link', error: err.message });
     }
-  });
-  
+});
+
 
 // Route to delete a file by ID (from both S3 and MongoDB)
 router.delete('/:id', authenticateToken(['admin']), async (req, res) => {
-  try {
-      const fileId = req.params.id;
+    try {
+        const fileId = req.params.id;
 
-      // Fetch the file metadata from MongoDB by ID
-      const fileRecord = await File.findById(fileId);
+        // Fetch the file metadata from MongoDB by ID
+        const fileRecord = await File.findById(fileId);
 
-      if (!fileRecord) {
-          return res.status(404).json({ message: 'File not found' });
-      }
+        if (!fileRecord) {
+            return res.status(404).json({ message: 'File not found' });
+        }
 
-      const s3Key = fileRecord.s3Key;
+        const s3Key = fileRecord.s3Key;
 
-      // Delete the file from S3
-      await s3Service.deleteFile(s3Key);
+        // Delete the file from S3
+        await s3Service.deleteFile(s3Key);
 
-      // Delete the file metadata from MongoDB
-      await File.findByIdAndDelete(fileId);
+        // Delete the file metadata from MongoDB
+        await File.findByIdAndDelete(fileId);
 
-      res.status(200).json({ message: 'File deleted successfully' });
-  } catch (err) {
-      console.error('Error deleting file:', err);
-      res.status(500).json({ message: 'Error deleting file', error: err.message });
-  }
+        res.status(200).json({ message: 'File deleted successfully' });
+    } catch (err) {
+        console.error('Error deleting file:', err);
+        res.status(500).json({ message: 'Error deleting file', error: err.message });
+    }
 });
 
 
 // Route to get file metadata by file ID
 router.get('/:id/metadata', authenticateToken(['member', 'admin']), async (req, res) => {
-  try {
-      const fileId = req.params.id;
+    try {
+        const fileId = req.params.id;
 
-      // Fetch the file metadata from MongoDB by ID
-      const fileRecord = await File.findById(fileId);
+        // Fetch the file metadata from MongoDB by ID
+        const fileRecord = await File.findById(fileId);
 
-      if (!fileRecord) {
-          return res.status(404).json({ message: 'File not found' });
-      }
+        if (!fileRecord) {
+            return res.status(404).json({ message: 'File not found' });
+        }
 
-      // Send file metadata (including the filename, tags, and categories)
-      res.status(200).json({
-          fileName: fileRecord.fileName,
-          fileType: fileRecord.fileType,
-          author: fileRecord.author,
-          uploadDate: fileRecord.uploadDate,
-          tags: fileRecord.tags, // Include tags in response
-          categories: fileRecord.categories // Include categories in response
-      });
-  } catch (err) {
-      console.error('Error retrieving file metadata:', err);
-      res.status(500).json({ message: 'Error retrieving file metadata', error: err.message });
-  }
+        // Send file metadata (including the filename, tags, and categories)
+        res.status(200).json({
+            fileName: fileRecord.fileName,
+            fileType: fileRecord.fileType,
+            author: fileRecord.author,
+            uploadDate: fileRecord.uploadDate,
+            tags: fileRecord.tags, // Include tags in response
+            categories: fileRecord.categories // Include categories in response
+        });
+    } catch (err) {
+        console.error('Error retrieving file metadata:', err);
+        res.status(500).json({ message: 'Error retrieving file metadata', error: err.message });
+    }
 });
 
 // Route to update file attributes (fileName, author, tags, categories) by file ID
 router.put('/:id', authenticateToken(['admin']), async (req, res) => {
-  try {
-      const fileId = req.params.id;
+    try {
+        const fileId = req.params.id;
 
-      // Find the file by ID
-      const fileRecord = await File.findById(fileId);
+        // Find the file by ID
+        const fileRecord = await File.findById(fileId);
 
-      if (!fileRecord) {
-          return res.status(404).json({ message: 'File not found' });
-      }
+        if (!fileRecord) {
+            return res.status(404).json({ message: 'File not found' });
+        }
 
-      // Update file attributes if provided in the request body
-      if (req.body.fileName) fileRecord.fileName = req.body.fileName;
-      if (req.body.author) fileRecord.author = req.body.author;
-      if (req.body.tags) fileRecord.tags = req.body.tags.split(',').map(tag => tag.trim());
-      if (req.body.categories) fileRecord.categories = req.body.categories.split(',').map(cat => cat.trim());
+        // Update file attributes if provided in the request body
+        if (req.body.fileName) fileRecord.fileName = req.body.fileName;
+        if (req.body.author) fileRecord.author = req.body.author;
+        if (req.body.tags) fileRecord.tags = req.body.tags.split(',').map(tag => tag.trim());
+        if (req.body.categories) fileRecord.categories = req.body.categories.split(',').map(cat => cat.trim());
 
-      // Save the updated record to the database
-      await fileRecord.save();
+        // Save the updated record to the database
+        await fileRecord.save();
 
-      // Send back the updated file record
-      res.status(200).json({ message: 'File attributes updated successfully', file: fileRecord });
-  } catch (err) {
-      console.error('Error updating file attributes:', err);
-      res.status(500).json({ message: 'Error updating file attributes', error: err.message });
-  }
+        // Send back the updated file record
+        res.status(200).json({ message: 'File attributes updated successfully', file: fileRecord });
+    } catch (err) {
+        console.error('Error updating file attributes:', err);
+        res.status(500).json({ message: 'Error updating file attributes', error: err.message });
+    }
 });
 
-  
+
 
 // Route to generate a presigned URL for accessing a file
 // router.get('/presigned-url', async (req, res) => {
@@ -208,55 +212,59 @@ router.put('/:id', authenticateToken(['admin']), async (req, res) => {
 // });
 
 // Route to list all files from the MongoDB model, excluding s3Key and s3Bucket
-router.get('/', authenticateToken(['member', 'admin']), async (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        // Create an empty filter object
         let filter = {};
 
-        // Check for query parameters and add them to the filter object if they exist
-        if (req.query.fileName) {
-            filter.fileName = { $regex: req.query.fileName, $options: 'i' }; // Partial match
-        }
-        if (req.query.author) {
-            filter.author = { $regex: req.query.author, $options: 'i' }; // Partial match
-        }
-        if (req.query.uploadDate) {
-            filter.uploadDate = new Date(req.query.uploadDate); // Exact match
-        }
-        if (req.query.fileSize) {
-            filter.fileSize = req.query.fileSize; // Exact match, you can add range logic here
-        }
-        if (req.query.fileType) {
-            filter.fileType = { $regex: req.query.fileType, $options: 'i' }; // Partial match
-        }
+        // Add query parameter filters
+        if (req.query.fileName) filter.fileName = { $regex: req.query.fileName, $options: 'i' };
+        if (req.query.author) filter.author = { $regex: req.query.author, $options: 'i' };
+        if (req.query.uploadDate) filter.uploadDate = new Date(req.query.uploadDate);
+        if (req.query.fileSize) filter.fileSize = req.query.fileSize;
+        if (req.query.fileType) filter.fileType = { $regex: req.query.fileType, $options: 'i' };
         if (req.query.tags) {
-            const tagList = req.query.tags.split(',').map(tag => new RegExp(tag.trim(), 'i')); // Convert to regex patterns
+            const tagList = req.query.tags.split(',').map(tag => new RegExp(tag.trim(), 'i'));
             filter.tags = { $in: tagList };
         }
         if (req.query.categories) {
-            const categoryList = req.query.categories.split(',').map(category => new RegExp(category.trim(), 'i')); // Convert to regex patterns
+            const categoryList = req.query.categories.split(',').map(category => new RegExp(category.trim(), 'i'));
             filter.categories = { $in: categoryList };
         }
 
-        // Initialize projection and sort options
-        let projection = { s3Key: 0, s3Bucket: 0 }; // Exclude these fields
+        // Get token and check user role
+        const token = req.header('Authorization')?.split(' ')[1];
+        if (token) {
+            try {
+                const user = jwt.verify(token, SECRET_KEY);
+                const foundUser = await User.findById(user.id);
+                if (foundUser?.roles.includes('admin')) {
+                    // Admin sees all files
+                } else {
+                    // Member sees only public files
+                    filter.visibility = 'public';
+                }
+            } catch (err) {
+                // Invalid token - treat as public user
+                filter.visibility = 'public';
+            }
+        } else {
+            // No token - public user sees only public files
+            filter.visibility = 'public';
+        }
+
+        let projection = { s3Key: 0, s3Bucket: 0 };
         let sortOption = {};
 
-        // Text search
         if (req.query.searchTerm) {
             filter.$text = { $search: req.query.searchTerm };
-
-            // Since we are using text search, include the textScore in projection and sort
             projection.score = { $meta: 'textScore' };
             sortOption.score = { $meta: 'textScore' };
         }
 
-        // Query the File model using the constructed filter
         const files = await File.find(filter)
             .select(projection)
             .sort(sortOption);
 
-        // Return the filtered list of files (without s3Key and s3Bucket)
         res.status(200).json({ files });
     } catch (err) {
         console.error('Error retrieving files:', err);
@@ -265,6 +273,6 @@ router.get('/', authenticateToken(['member', 'admin']), async (req, res) => {
 });
 
 
-  
+
 
 module.exports = router;
