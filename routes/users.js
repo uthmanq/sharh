92d5660100e3a3d82afad236c1df1ff5c0d3ad38
Uthman Qureshi <uthman.aq@gmail.com>
2025-03-06 12:30:17 -0500

    added testing configurations to account creation too

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,8 @@ const User = require('../models/User');
 const authenticateToken = require('../middleware/authenticate'); // Use your existing middleware
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripeConfig = require('../config/stripeConfig')
+const stripe = require('stripe')(stripeConfig.secretKey);
 const SECRET_KEY = process.env.SECRET_KEY;
 const { sendEmail } = require('../scripts/sendEmail');
 
