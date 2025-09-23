ee09f87a39451bb0fc2f5fd8e46b4a3832563646
Uthman Qureshi <uthman.aq@gmail.com>
2025-09-23 10:27:01 -0400

    added

--- services/QuizService.js
+++ services/QuizService.js
@@ -2,7 +2,7 @@ const { GoogleGenerativeAI } = require('@google/generative-ai');
 require('dotenv').config({ path: '../.env' });
 const { Quiz } = require('../models/QuizModel.js');
 
-console.log(rocess.env.GEMINI_API_KEY);
+console.log(process.env.GEMINI_API_KEY);
 
 const genAI = new GoogleGenerativeAI({ apiKey: process.env.GEMINI_API_KEY});
 
